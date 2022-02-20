af1afaa214814bcca2bd966dfc4683f8319d4af0
ImACyborgButThatsOK <buhhdz@gmail.com>
2022-02-20 00:39:26 -0300

    alterando-arquivo-data-da-lista-de-animes

--- src/components/List/data.js
+++ src/components/List/data.js
@@ -1,54 +1,118 @@
 export const animesList = [
   {
     id: "1",
-    image: "https://images6.alphacoders.com/319/thumb-1920-319175.jpg",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327806/anime/listAnime/one_piece_sxcc3h.jpg",
     title: "One piece",
   },
 
   {
     id: "2",
-    image: "https://images4.alphacoders.com/727/thumb-1920-727970.jpg",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/fullMetal_iubmka.jpg",
     title: "FullMetal Alchemist",
   },
 
   {
     id: "3",
-    image: "https://images6.alphacoders.com/353/thumb-1920-353759.png",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327806/anime/listAnime/SteinsGate_nqdzti.png",
     title: "Steins; Gate",
   },
 
   {
     id: "4",
-    image: "https://images4.alphacoders.com/840/thumb-1920-840709.jpg",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/cowboyBebop_dynpyc.jpg",
     title: "Cowboy Bebop",
   },
 
   {
     id: "5",
-    image: "https://images3.alphacoders.com/713/thumb-1920-713064.png",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/clannad_sciz2w.png",
     title: "CLANNAD",
   },
 
   {
     id: "6",
-    image: "https://images2.alphacoders.com/653/thumb-1920-653548.jpg",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/kizumonogatari_vc4c79.jpg",
     title: "Kizumonogatari",
   },
 
   {
     id: "7",
-    image: "https://images.alphacoders.com/560/thumb-1920-560960.png",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/Hunter_xHunter_tzjvfw.png",
     title: "Hunter x Hunter",
   },
   {
     id: "8",
-    image: "https://images3.alphacoders.com/114/thumb-1920-1140983.jpg",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327805/anime/listAnime/jujutsukaisen_tgysri.jpg",
     title: "Jujutsu Kaisen",
   },
 
   {
     id: "9",
-    image: "https://images5.alphacoders.com/735/thumb-1920-735434.png",
+    image:
+      "https://res.cloudinary.com/duqlqfvdq/image/upload/v1645327806/anime/listAnime/sangatsu_dfskpw.png",
     title: "Sangatsu no Lion",
   },
 ];
+
+// export const animesList = [
+//   {
+//     id: "1",
+//     image: "https://images6.alphacoders.com/319/thumb-1920-319175.jpg",
+//     title: "One piece",
+//   },
+
+//   {
+//     id: "2",
+//     image: "https://images4.alphacoders.com/727/thumb-1920-727970.jpg",
+//     title: "FullMetal Alchemist",
+//   },
+
+//   {
+//     id: "3",
+//     image: "https://images6.alphacoders.com/353/thumb-1920-353759.png",
+//     title: "Steins; Gate",
+//   },
+
+//   {
+//     id: "4",
+//     image: "https://images4.alphacoders.com/840/thumb-1920-840709.jpg",
+//     title: "Cowboy Bebop",
+//   },
+
+//   {
+//     id: "5",
+//     image: "https://images3.alphacoders.com/713/thumb-1920-713064.png",
+//     title: "CLANNAD",
+//   },
+
+//   {
+//     id: "6",
+//     image: "https://images2.alphacoders.com/653/thumb-1920-653548.jpg",
+//     title: "Kizumonogatari",
+//   },
+
+//   {
+//     id: "7",
+//     image: "https://images.alphacoders.com/560/thumb-1920-560960.png",
+//     title: "Hunter x Hunter",
+//   },
+//   {
+//     id: "8",
+//     image: "https://images3.alphacoders.com/114/thumb-1920-1140983.jpg",
+//     title: "Jujutsu Kaisen",
+//   },
+
+//   {
+//     id: "9",
+//     image: "https://images5.alphacoders.com/735/thumb-1920-735434.png",
+//     title: "Sangatsu no Lion",
+//   },
+// ];
